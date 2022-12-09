097037806b890fa50c447cc59f2d8bd5b39ad9c1
Mani Kumar <manid2.work@gmail.com>
2022-12-09 10:06:41 +0530

    Add string to odd even nums code

---
+++ string-to-odd-even-nums.cpp
@@ -0,0 +1,57 @@
+// Given a vector of strings representing unsigned numbers, convert the numbers to
+// a collection of std::uint16_t in which the odd numbers appear before the even ones
+// and output the data
+
+// feel free to use https://en.cppreference.com/
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <stdexcept>
+
+auto input = std::vector<std::string>{"9", "0", "49", "2", "100", "not a number" , "12 not a number too"};
+
+bool is_number(const std::string &s) {
+	return !s.empty() && std::all_of(s.begin(), s.end(), ::isdigit);
+}
+
+std::vector<std::uint16_t> getOddEvenNums (const std::vector<std::string> &input) {
+	std::vector<std::uint16_t> oen;
+	for (auto &a : input) {
+		std::uint16_t d = 0;
+		try {
+			if (is_number (a)) {
+				d = static_cast<std::uint16_t>(std::stoul (a));
+				oen.push_back (d);
+			}
+		}
+		catch(std::invalid_argument const& ex) {
+			std::cout << "std::invalid_argument::what(): " << ex.what() << '\n';
+		}
+		catch(std::out_of_range const& ex) {
+			std::cout << "std::out_of_range::what(): " << ex.what() << '\n';
+		}
+	}
+
+	std::size_t s = 0;
+	std::size_t e = oen.size() - 1;
+	while (s < e) {
+		while (oen.at(s) % 2 == 0 && s < e) s++;
+		while (oen.at(e) % 2 != 1 && s < e) e--;
+		// similar to bubble sort swap odd & even numbers
+		if (s < e) {
+			std::swap (oen.at(s), oen.at(e));
+			s++;
+			e--;
+		}
+	}
+	return oen;
+}
+
+int main(int ac, char **av) {
+	std::vector<std::uint16_t> oen = getOddEvenNums(input);
+	for (auto &a : oen) {
+		std::cout << a << " ";
+	}
+	return 0;
+}
