fd29f0eacf532025e0a12b41b20570c1b8fd5857
Mani Kumar <manid2.work@gmail.com>
2024-01-08 13:07:12 +0530

    feat: [cpp] Add reverse LL in K groups

---
+++ cpp/linked-list-reverse-in-k-groups.cpp
@@ -0,0 +1,81 @@
+/* Reverse linked list iterative */
+ListNode* reverse(ListNode *head) {
+	ListNode *prev, *next, *curr;
+
+	prev = next = nullptr;
+	curr = head;
+
+	while (curr) {
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+
+	return curr;
+}
+
+/**
+ * Reverse a linked list in k groups
+ * =================================
+ *
+ * Leetcode: [25. Reverse Nodes in k-Group][lc25]
+ *
+ * [lc25]: https://leetcode.com/problems/reverse-nodes-in-k-group/
+ *
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    /**
+     * Reverse a linked list in k groups iterative solution
+     */
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(!head || !head->next || k < 2)
+            return head;
+
+        ListNode dummy(0);
+        ListNode *prev_head = &dummy;
+        prev_head->next = head;
+
+        for(;;) {
+            int ck = 0;
+            ListNode *t = prev_head->next;
+
+            while (t) {
+                if (++ck == k)
+                    break;
+                t = t->next;
+            }
+
+            if (ck < k)
+                break;
+
+            ListNode *next_head = prev_head->next;
+            ListNode *prev, *curr, *next;
+
+            prev = next = nullptr;
+            curr = next_head;
+
+            for(int i = 0; i < k; i++) {
+                next = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = next;
+            }
+
+            prev_head->next = prev;
+            next_head->next = curr;
+            prev_head = next_head;
+        }
+
+        return dummy.next;
+    }
+};
