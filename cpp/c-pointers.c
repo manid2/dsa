dee7e5bad9c51b91ea5d09b8df04132fe56d9fd5
Mani Kumar <manid2.work@gmail.com>
2024-01-19 14:46:36 +0530

    feat: [c] Add pointers

---
+++ cpp/c-pointers.c
@@ -0,0 +1,24 @@
+/**
+ * Pointer operators precedence
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int *ptr = (int *) malloc(5 * sizeof(int));
+	int *p = ptr;
+
+	for (int i = 0; i < 5; i++)
+		*(ptr + i) = i;
+
+	printf("%d ", *ptr++);
+	printf("%d ", (*ptr)++);
+	printf("%d ", *ptr);
+	printf("%d ", *++ptr);
+	printf("%d ", ++*ptr);
+
+	free (p);
+	return 0;
+}
