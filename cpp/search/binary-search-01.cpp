656035e625e283482f5a7cef9d3c455229a93262
Mani Kumar <manid2.work@gmail.com>
2024-08-26 00:05:02 +0530

    refactor: [search] Use tests.h

--- cpp/search/binary-search-01.cpp
+++ cpp/search/binary-search-01.cpp
@@ -9,112 +9,57 @@
  * https://leetcode.com/problems/binary-search/
  */
 
-#include <bits/stdc++.h>
-
-using namespace std;
-
-/* ===========================================================================
- * Test helpers
- * ===========================================================================
- */
-class _00_test
-{
-public:
-	_00_test(const string &name) : name(name) {}
-
-	string getName(void) const { return name; }
-
-	virtual int search(vector<int> &nums, int target) = 0;
-
-private:
-	string name;
-};
+#include "tests.h"
 
 /* ===========================================================================
  * Algorithms implementation
  * ===========================================================================
  */
 
+#define _bs_desc "Binary search"
+
 /* TC : Best O(1), Average O(log n), Worst O(log n)
  * SC : O(1)
  */
-class _01_binary_search : public _00_test
+int search(const vi_t &a, int t)
 {
-public:
-	_01_binary_search() : _00_test("Binary search") {}
-
-	int search(vector<int> &a, int t) override
-	{
-		int l = 0, r = a.size() - 1;
-		while (l <= r) {
-			int m = (l + r) / 2;
-			if (t == a[m])
-				return m;
-			else if (t < a[m])
-				r = m - 1;
-			else
-				l = m + 1;
-		}
-		return -1;
+	int n = size(a), l = 0, r = n - 1;
+	while (l <= r) {
+		int m = (l + r) / 2;
+		if (t == a[m])
+			return m;
+		else if (t < a[m])
+			r = m - 1;
+		else
+			l = m + 1;
 	}
-};
+	return -1;
+}
 
 /* ===========================================================================
  * Test code
  * ===========================================================================
  */
-string _vec2str(vector<int> &vec)
-{
-	ostringstream oss;
-	oss << "{";
-	copy(vec.begin(), vec.end() - 1, ostream_iterator<int>(oss, ", "));
-	oss << vec.back();
-	oss << "}";
-	return oss.str();
-}
-
-void test_impl(vector<pair<vector<int>, int>> &ip, vector<int> &op,
-               shared_ptr<_00_test> f)
-{
-	for (size_t i = 0; i < ip.size(); i++) {
-		int t = f->search(ip[i].first, ip[i].second);
-		if (t != op[i]) {
-			cerr << f->getName() << " test failed: "
-			     << "expected " << op[i] << ", actual " << t
-			     << "." << endl;
-			exit(1);
-		}
-
-		if (getenv("SHOW_TEST_OUTPUT"))
-			cout << "  test-" << i << ":  "
-			     << "input: nums = " << _vec2str(ip[i].first)
-			     << ", target = " << ip[i].second
-			     << "  output: index = " << t << "\n";
-	}
-}
-
-int main(int, char **)
-{
-	vector<pair<vector<int>, int>> ip{
-	    {{-1, 0, 3, 5, 9, 12}, 9},
-	    {{-1, 0, 3, 5, 9, 12}, 2},
-	};
-
-	vector<int> op{4, -1};
-
-	vector<shared_ptr<_00_test>> impls{
-	    make_shared<_01_binary_search>(),
-	};
-
-	for (size_t i = 0; i < impls.size(); i++) {
-		if (getenv("SHOW_TEST_OUTPUT"))
-			cout << "Testing implementation " << i + 1 << " "
-			     << impls[i]->getName() << "\n";
-
-		test_impl(ip, op, impls[i]);
+#define _bs_check(arr, target, e)                                            \
+	{                                                                    \
+		int a = search(arr, target);                                 \
+		string im = format("array = {}, target = {}",                \
+		                   to_string(arr), target);                  \
+		SET_CUSTOM_SUCCESS_MSG(im, to_string(a));                    \
+		CHECK_EQ(e, a);                                              \
+		SHOW_OUTPUT(im, a);                                          \
 	}
 
-	cout << "Executed " << impls.size() << " implementations"
-	     << " with " << ip.size() << " tests." << endl;
-	return 0;
+TEST(search, _bs_desc)
+{
+	vi2_t arrs{
+	    {-1, 0, 3, 5, 9, 12},
+	    {-1, 0, 3, 5, 9, 12},
+	};
+	vi_t targets{9, 2};
+	vi_t exps{4, -1};
+	int n = size(arrs);
+	fii (i, n) _bs_check(arrs[i], targets[i], exps[i]);
 }
+
+INIT_TEST_MAIN();
--- cpp/search/binary-search-03.cpp
+++ cpp/search/binary-search-03.cpp
@@ -9,127 +9,70 @@
  * https://leetcode.com/problems/search-in-rotated-sorted-array
  */
 
-#include <bits/stdc++.h>
-
-using namespace std;
-
-/* ===========================================================================
- * Test helpers
- * ===========================================================================
- */
-class _00_test
-{
-public:
-	_00_test(const string &name) : name(name) {}
-
-	string getName(void) const { return name; }
-
-	virtual bool search(vector<int> &nums, int target) = 0;
-
-private:
-	string name;
-};
+#include "tests.h"
 
 /* ===========================================================================
  * Algorithms implementation
  * ===========================================================================
  */
 
+#define _bs_desc "Binary search in sorted rotated array with duplicates"
+
 /* TC : Best O(1), Average O(log n), Worst O(log n)
  * SC : O(1)
  */
-class _01_binary_search : public _00_test
+bool search(const vi_t &a, int t)
 {
-public:
-	_01_binary_search()
-	    : _00_test(
-	          "Binary search in sorted rotated array with duplicates")
-	{
-	}
-
-	bool search(vector<int> &a, int t) override
-	{
-		int l = 0, r = a.size() - 1;
-		while (l <= r) {
-			int m = (l + r) / 2;
-			if (t == a[m]) return true;
-			if (a[l] == a[m] && a[m] == a[r]) {
-				l++, r--;
-				continue;
-			}
-			if (a[l] <= a[m]) {
-				if (a[l] <= t && t < a[m])
-					r = m - 1;
-				else
-					l = m + 1;
-			} else {
-				if (a[m] < t && t <= a[r])
-					l = m + 1;
-				else
-					r = m - 1;
-			}
+	int n = size(a), l = 0, r = n - 1;
+	while (l <= r) {
+		int m = (l + r) / 2;
+		if (t == a[m]) return true;
+		if (a[l] == a[m] && a[m] == a[r]) {
+			l++, r--;
+			continue;
 		}
-		return false;
+		if (a[l] <= a[m]) {
+			if (a[l] <= t && t < a[m])
+				r = m - 1;
+			else
+				l = m + 1;
+		} else {
+			if (a[m] < t && t <= a[r])
+				l = m + 1;
+			else
+				r = m - 1;
+		}
 	}
-};
+	return false;
+}
 
 /* ===========================================================================
  * Test code
  * ===========================================================================
  */
-string _vec2str(vector<int> &vec)
-{
-	ostringstream oss;
-	copy(vec.begin(), vec.end() - 1, ostream_iterator<int>(oss, ", "));
-	oss << vec.back();
-	return oss.str();
-}
-
-void test_impl(vector<pair<vector<int>, int>> &ip, vector<int> &op,
-               shared_ptr<_00_test> f)
-{
-	for (size_t i = 0; i < ip.size(); i++) {
-		bool t = f->search(ip[i].first, ip[i].second);
-		if (t != op[i]) {
-			cerr << f->getName() << " test failed: "
-			     << "expected " << op[i] << ", actual " << t
-			     << "." << endl;
-			exit(1);
-		}
-
-		if (getenv("SHOW_TEST_OUTPUT"))
-			cout << "  test-" << i << ":  "
-			     << "input: nums = " << _vec2str(ip[i].first)
-			     << ", target = " << ip[i].second
-			     << "  output: found = " << boolalpha << t
-			     << "\n";
-	}
-}
-
-int main(int, char **)
-{
-	vector<pair<vector<int>, int>> ip{
-	    {{4, 5, 6, 7, 0, 1, 2}, 0},
-	    {{2, 5, 6, 0, 0, 1, 2}, 3},
-	    {{1}, 0},
-	    {{3, 1, 2, 3, 3, 3, 3}, 1},
-	};
-
-	vector<int> op{true, false, false, true};
-
-	vector<shared_ptr<_00_test>> impls{
-	    make_shared<_01_binary_search>(),
-	};
-
-	for (size_t i = 0; i < impls.size(); i++) {
-		if (getenv("SHOW_TEST_OUTPUT"))
-			cout << "Testing implementation " << i + 1 << " "
-			     << impls[i]->getName() << "\n";
-
-		test_impl(ip, op, impls[i]);
+#define _bs_check(arr, target, e)                                            \
+	{                                                                    \
+		bool a = search(arr, target);                                \
+		string im = format("array = {}, target = {}",                \
+		                   to_string(arr), target);                  \
+		string om = a ? "true" : "false";                            \
+		SET_CUSTOM_SUCCESS_MSG(im, om);                              \
+		CHECK_EQ(e, a);                                              \
+		SHOW_OUTPUT(im, a);                                          \
 	}
 
-	cout << "Executed " << impls.size() << " implementations"
-	     << " with " << ip.size() << " tests." << endl;
-	return 0;
+TEST(search, _bs_desc)
+{
+	vi2_t arrs{
+	    {4, 5, 6, 7, 0, 1, 2},
+	    {2, 5, 6, 0, 0, 1, 2},
+	    {1},
+	    {3, 1, 2, 3, 3, 3, 3},
+	};
+	vi_t targets{0, 3, 0, 1};
+	vi_t exps{true, false, false, true};
+	int n = size(arrs);
+	fii (i, n) _bs_check(arrs[i], targets[i], exps[i]);
 }
+
+INIT_TEST_MAIN();
