93363b986c68510fa6d14eb3678aa067bd0c0232
Mani Kumar <manid2.work@gmail.com>
2024-09-25 17:56:52 +0530

    wip: Add one producer one consumer

--- cpp/ipc/pc1-one-producer-one-consumer.cpp
+++ cpp/ipc/pc1-one-producer-one-consumer.cpp
@@ -8,67 +8,82 @@
  */
 
 #include "tests.h"
+#include <syncstream>
 
 /* ===========================================================================
  * Algorithms implementation
  * ===========================================================================
  */
+#define _pc1_MutexCondVar_desc "Mutex & Conditional Variable"
 
-// Shared buffer and synchronization primitives
-queue<pair<int, double>> buffer;
-mutex mtx;
+namespace MutexCondVar
+{
+/* threads synchronization primitives */
+mutex mx;
 condition_variable cv;
-bool done = false;
 
-// Producer function
-void producer()
+/* shared queue between threads */
+queue<int> sq;
+
+void producer(int id, osyncstream &out)
 {
-	cout << "Producer: Running\n";
-	random_device rd;
-	mt19937 gen(rd());
-	uniform_real_distribution<> dis(0.0, 1.0);
-
-	for (int i = 0; i < 10; ++i) {
-		double value = dis(gen);
-		this_thread::sleep_for(chrono::duration<double>(value));
-		unique_lock<mutex> lock(mtx);
-		buffer.push({i, value});
-		cout << ">producer added (" << i << ", " << value << ")\n";
-		cv.notify_all();
-	}
-
-	unique_lock<mutex> lock(mtx);
-	done = true;
-	cv.notify_all();
-	cout << "Producer: Done\n";
-}
-
-// Consumer function
-void consumer()
-{
-	cout << "Consumer: Running\n";
-	while (true) {
-		unique_lock<mutex> lock(mtx);
-		cv.wait(lock, [] { return !buffer.empty() || done; });
-		if (!buffer.empty()) {
-			auto item = buffer.front();
-			buffer.pop();
-			lock.unlock();
-			this_thread::sleep_for(
-			    chrono::duration<double>(item.second));
-			cout << ">consumer got (" << item.first << ", "
-			     << item.second << ")\n";
-		} else if (done) {
-			break;
+	string name = format("producer {}", id);
+	vi_t a(5);
+	iota(begin(a), end(a), 0);
+	a.push_back(-1); // sentinel object to indicate end of items
+	for (int i : a) {
+		{
+			lock_guard<mutex> lk(mx);
+			sq.push(i); // produce 1 item
+			out << format("{}: write {}", name, i);
 		}
+		cv.notify_one();
 	}
-	cout << "Consumer: Done\n";
 }
+
+void consumer(int id, osyncstream &out)
+{
+	string name = format("consumer {}", id);
+	for (;;) {
+		unique_lock<mutex> lk(mx);
+		cv.wait(lk, []() { return !empty(sq); });
+		int i = sq.front(); // consume 1 item
+		sq.pop();
+		out << format("{}: read {}", name, i);
+		if (i == -1) break; // exit on finding sentinel object
+	}
+}
+} // namespace MutexCondVar
 
 /* ===========================================================================
  * Test code
  * ===========================================================================
  */
-_pc1_test(MutexCondVar);
+#define _pc1_desc_prefix "Threads: One producer one consumer"
+
+#define _PC1_NAME(var) var
+#define _PC1_DESC(var) _pc1_desc_prefix " - " _pc1_##var##_desc
+
+#define _PC1_TEST(var)                                                       \
+	TEST(_PC1_NAME(var), _PC1_DESC(var))                                 \
+	{                                                                    \
+		/* synchronized stream */                                    \
+		osyncstream out(ostringstream);                              \
+		using namespace _PC1_NAME(var);                              \
+		int np = 1, nc = 1;                                          \
+		vector<thread> producers(np);                                \
+		vector<thread> consumers(nc);                                \
+		producers.push_back(thread(producer, np--, out));            \
+		consumers.push_back(thread(consumer, nc--, out));            \
+		for (auto &x : producers) x.join();                          \
+		for (auto &x : consumers) x.join();                          \
+		out.emit();                                                  \
+		SUCCESS();                                                   \
+		string im = format("producers {}, consumers {}", np, nc);    \
+		string om = out.get_wrapped().str();                         \
+		SHOW_OUTPUT(im, om);                                         \
+	}
+
+_PC1_TEST(MutexCondVar);
 
 INIT_TEST_MAIN();
